7248b34e1564fe4bdef6cf4f0414d5d95c59df2c
arcolinuxz <arcolinuxinfo@gmail.com>
2024-05-24 10:20:46 +0200

    archiso

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -25,7 +25,7 @@ echo "################################################################## "
 echo
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 77-1"
+	archisoRequiredVersion="archiso 78-1"
 	buildFolder=$HOME"/alci-build"
 	outFolder=$HOME"/Alci-Iso-Dev-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
